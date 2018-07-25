9d03b07f165d6165b2812f07a06f0a979133ed2b
starryknight <mrstanleykimani@gmail.com>
2018-07-25 18:07:26 -0400

    made changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :tribes do
+
+    resources :users do
       resources :characters
       end
-    end
-   resources :users do
+    
+   resources :tribes do
     resources :characters
     end
   end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
