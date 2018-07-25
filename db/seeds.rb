5901690f2331496a6ae9a59d0fdb8df6fa22d3cb
starryknight <mrstanleykimani@gmail.com>
2018-07-25 14:32:28 -0400

    made seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tribe.destroy_all
+User.destroy_all
+
+stanley = User.create(username: "stanley",  password: "stanley001")
+
+druids = Tribe.create(tribe_name: "Druids", body_paint: "yellow", crest: "http://www.createmepink.com/wp-content/uploads/st/thumb-stock-illustration-vector-bull-cow-skull-horns-texas-themed-logo-image.jpg", village: "southgate E-5")
+
+Character.create(user_id: stanley.id, tribe_id: druids.id, character_name: "cratum", status: "peasant", weapon: "http://flyff-wiki.webzen.com/images/0/04/Angels_Sword.png", avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKAbEHNaAP6Iawo470QXix-miB6iA9ufV39Rj7t3ufYj1ujKH")
