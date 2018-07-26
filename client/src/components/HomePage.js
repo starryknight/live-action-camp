a757a1e50b3a13c55678837ff91b77c2260f3f43
starryknight <mrstanleykimani@gmail.com>
2018-07-26 17:47:09 -0400

    added semantic log in page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
 
 class HomePage extends Component {
     state = {
@@ -16,7 +17,7 @@ class HomePage extends Component {
           await this.setState({ users: res.data });
           return res.data;
         }
-        
+
         catch (err) {
           console.log(err)
           await this.setState({ error: err.message })
@@ -33,7 +34,45 @@ class HomePage extends Component {
         return (
             <div>
                 <h1>{userName}</h1>
+                <div className='login-form'>
+    
+    <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' textAlign='center'>
+          <Image src='/logo.png' /> Log-in to your account
+        </Header>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+
+            <Button color='teal' fluid size='large'>
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href='#'>Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
+
             </div>
+            
         );
     }
 }
