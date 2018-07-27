cf50ba6c9dfaeae078fdda80b3ce832b14481a23
starryknight <mrstanleykimani@gmail.com>
2018-07-27 11:27:35 -0400

    re installed modal

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,206 +1,106 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom'
-import { Button, Select, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import {
+  Button,
+  Modal,
+  Select,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
+import NewUserPage from "./NewUserPage";
 
 class HomePage extends Component {
-    state = {
-        users:[],
-        redirectToUserPage: false,
-        redirectPathname: ""
+  state = {
+    users: [],
+    redirectToUserPage: false,
+    redirectPathname: ""
+  };
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = async () => {
+    try {
+      const res = await axios.get("/api/users");
+      await this.setState({ users: res.data });
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      await this.setState({ error: err.message });
+      return err.message;
     }
+  };
 
-    componentDidMount(){
-        this.getUser()
+  handleRedirect = userId => {
+    this.setState({
+      redirectToUserPage: true,
+      redirectPathname: `/users/${userId}`
+    });
+  };
+
+  render() {
+    if (this.state.redirectToUserPage) {
+      return <Redirect to={this.state.redirectPathname} />;
     }
-
-    getUser = async () => {
-        try {
-          const res = await axios.get('/api/users');
-          await this.setState({ users: res.data });
-          return res.data;
-        }
-
-        catch (err) {
-          console.log(err)
-          await this.setState({ error: err.message })
-          return err.message
-        }
-      }
-
-      handleRedirect = (userId) => {
-        this.setState({
-            redirectToUserPage: true,
-            redirectPathname: `/users/${userId}`
-        })
-      }
-
-    render() {
-        if (this.state.redirectToUserPage) {
-            return (
-                <Redirect to={this.state.redirectPathname} />
-            );
-        }
-        const userName = this.state.users.map((user) => {
-            return(
-                <h1>{user.username}</h1>
-            );
-        })
-        return (
-            <div>
-                <h1>{userName}</h1>
-                <div className='login-form'>
-    
-    <style>{`
+    const userName = this.state.users.map(user => {
+      return <h1>{user.username}</h1>;
+    });
+    return (
+      <div>
+        <h1>{userName}</h1>
+        <div className="login-form">
+          <style>{`
       body > div,
       body > div > div,
       body > div > div > div.login-form {
         height: 100%;
       }
     `}</style>
-    
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='teal' textAlign='center'>
-           Live Action Camp
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-          
-            
-          <select name="" id="">
-          {
-              this.state.users.map((currentUser, index) => {
-                  return (
-                      <option key={index} onClick={() => this.handleRedirect(currentUser.id)}>{currentUser.username}</option>
-                  );
-              })
-          }
-          </select>
 
-          </Segment>
-        </Form>
-        <Message>
-          New to us? <a href='#'>Sign Up</a>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  </div>
-
-            </div>
-            
-        );
-    }
+          <Grid
+            textAlign="center"
+            style={{ height: "100%" }}
+            verticalAlign="middle"
+          >
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as="h2" color="teal" textAlign="center">
+                Live Action Camp
+              </Header>
+              <Form size="large">
+                <Segment stacked>
+                  <select name="" id="">
+                    {this.state.users.map((currentUser, index) => {
+                      return (
+                        <option
+                          key={index}
+                          onClick={() => this.handleRedirect(currentUser.id)}
+                        >
+                          {currentUser.username}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </Segment>
+              </Form>
+              <Message>
+                New to us?
+                <Modal trigger={<Button>Show Modal</Button>}>
+                  <NewUserPage />                  
+                </Modal>
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
 
-
-
-
-
-
-
-
-
-
-// class HomePage extends Component {
-//     state = {
-//         users:[],
-//         redirectToUserPage: false,
-//         redirectPathname: ""
-//     }
-
-//     componentDidMount(){
-//         this.getUser()
-//     }
-
-//     getUser = async () => {
-//         try {
-//           const res = await axios.get('/api/users');
-//           await this.setState({ users: res.data });
-//           return res.data;
-//         }
-
-//         catch (err) {
-//           console.log(err)
-//           await this.setState({ error: err.message })
-//           return err.message
-//         }
-//       }
-
-//       handleRedirect = () => {
-//         console.log("HIT")
-//             // redirectPathname: `/users/${userId}`
-//         const selectUser = Document.getElementById("selectUser");
-//         const userId = selectUser.options[selectUser.selectedIndex].value;
-//         console.log(userId)
-//         this.setState({
-//             redirectToUserPage: true,
-//             redirectPathname: `/users/${userId}`
-//         })
-//       }
-
-//     render() {
-//         if (this.state.redirectToUserPage) {
-//             return (
-//                 <Redirect to={this.state.redirectPathname} />
-//             );
-//         }
-//         const userName = this.state.users.map((user) => {
-//             return(
-//                 <h1>{user.username}</h1>
-//             );
-//         })
-//         return (
-//             <div>
-//     {/* //             <h1>{userName}</h1>
-//     //             <div className='login-form'>
-    
-//     // <style>{`
-//     //   body > div,
-//     //   body > div > div,
-//     //   body > div > div > div.login-form {
-//     //     height: 100%;
-//     //   }
-//     // `}</style> */}
-    
-//     <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-//       <Grid.Column style={{ maxWidth: 450 }}>
-//         <Header as='h2' color='teal' textAlign='center'>
-//            Live Action Camp
-//         </Header>
-//         <Form size='large'>
-//           <Segment stacked>
-          
-//           <select name="" id="selectUser" onChange={this.handleRedirect}>
-//           {
-//               this.state.users.map((currentUser, index) => {
-//                   return (
-//                       <option key={index} value={currentUser.id}>{currentUser.username}</option>
-//                   );
-//               })
-//           }
-//           </select>
-          
-
-//           </Segment>
-//         </Form>
-//         <Message>
-//           New to us? <Modal trigger={<Button>Show Modal</Button>}>
-//     <Modal.Header>Select a Photo</Modal.Header>
-//     <NewUserPage />
-//   </Modal>
-//         </Message>
-//       </Grid.Column>
-//     </Grid>
-//   </div>
-
-//             // </div>
-            
-//         );
-//     }
-// }
-
-// export default HomePage;
